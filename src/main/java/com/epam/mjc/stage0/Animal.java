3d26f88fd5cc6b3a4298b29f2bf101e4eeea3fc3
MarsX-2002 <mirjalol0331@gmail.com>
2023-04-10 21:23:54 +0500

    OOP

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,33 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean getHasFur() {
+        return hasFur;
+    }
+
+    public String getDescription() {
+        String temp = hasFur ? "a" : "no";
+        String paws = numberOfPaws > 1 ? "paws" : "paw";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + paws + " and " + temp + " fur.";
+    }
 }
+
+
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,9 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
 
+    Dog(String color, int numberOfPaws, boolean hasFur) {
+        super("brown", 4, true);
+
+    }
 }
